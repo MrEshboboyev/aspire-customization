f079ac0a2baf4ac1961c7b81d8dc50d98f000171
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-24 20:35:21 +0500

    Custom Commands implementation

--- Aspire.Customization.AppHost/Program.cs
+++ Aspire.Customization.AppHost/Program.cs
@@ -1,46 +1,12 @@
+using Aspire.Customization.AppHost;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Diagnostics;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var cache = builder.AddRedis("Cache");
-
-var apiService = builder.AddProject<Projects.ApiService>("apiservice");
-
-var name = "swagger-ui-docs";
-var displayName = "Swagger UI Documentation";
-var openApiUiPath = "swagger";
-apiService.WithCommand(
-    name,
-    displayName,
-    executeCommand: async _ =>
-    {
-        try
-        {
-            // Base URL
-            var endpoint = apiService.GetEndpoint("https");
-
-            var url = $"{endpoint.Url}/{openApiUiPath}";
-
-            Process.Start(new ProcessStartInfo(url)
-            {
-                UseShellExecute = true,
-            });
-
-            return new ExecuteCommandResult { Success = true};
-        }
-        catch (Exception ex)
-        {
-            return new ExecuteCommandResult 
-            { 
-                Success = false, 
-                ErrorMessage = ex.ToString() 
-            };
-        }
-    }, 
-    updateState: context => context.ResourceSnapshot.HealthStatus == HealthStatus.Healthy
-        ? ResourceCommandState.Enabled : ResourceCommandState.Disabled,
-    iconName: "Document",
-    iconVariant: IconVariant.Filled);
+builder.AddProject<Projects.ApiService>("apiservice")
+    .WithSwaggerUI()
+    .WithScalar()
+    .WithReDoc();
 
 builder.Build().Run();
---
+++ Aspire.Customization.AppHost/ResourceBuilderExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace Aspire.Customization.AppHost;
+
+internal static class ResourceBuilderExtensions
+{
+    internal static IResourceBuilder<T> WithSwaggerUI<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+    {
+        return builder.WithOpenApiDocs("swagger-ui-docs", "Swagger UI Documentation", "swagger");
+    }
+
+    internal static IResourceBuilder<T> WithScalar<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+    {
+        return builder.WithOpenApiDocs("scalar-ui-docs", "Scalar UI Documentation", "scalar/v1");
+    }
+
+    internal static IResourceBuilder<T> WithReDoc<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+    {
+        return builder.WithOpenApiDocs("redoc-ui-docs", "ReDoc UI Documentation", "api-docs");
+    }
+
+    internal static IResourceBuilder<T> WithOpenApiDocs<T>(this IResourceBuilder<T> builder,
+        string name,
+        string displayName,
+        string openApiUiPath)
+        where T : IResourceWithEndpoints 
+    {
+        return builder.WithCommand(
+            name,
+            displayName,
+            executeCommand: async _ =>
+            {
+                try
+                {
+                    // Base URL
+                    var endpoint = builder.GetEndpoint("https");
+
+                    var url = $"{endpoint.Url}/{openApiUiPath}";
+
+                    Process.Start(new ProcessStartInfo(url)
+                    {
+                        UseShellExecute = true,
+                    });
+
+                    return new ExecuteCommandResult { Success = true };
+                }
+                catch (Exception ex)
+                {
+                    return new ExecuteCommandResult
+                    {
+                        Success = false,
+                        ErrorMessage = ex.ToString()
+                    };
+                }
+            },
+            updateState: context => context.ResourceSnapshot.HealthStatus == HealthStatus.Healthy
+                ? ResourceCommandState.Enabled : ResourceCommandState.Disabled,
+            iconName: "Document",
+            iconVariant: IconVariant.Filled);
+            }
+}
